d0a4924a54b6b9e1829fb255e5aa3cbe4f59d63b
Andrew <andrew@freebsd-dev.lan>
2024-01-24 10:48:40 -0800

    2015 day 01 part 2 solution

--- 2015/01/main.zig
+++ 2015/01/main.zig
@@ -13,10 +13,33 @@ pub fn part_1(input: []const u8) i64 {
     return floor;
 }
 
+pub fn part_2(input: []const u8) usize {
+    var floor: i64 = 0;
+    for (input, 1..) |c, i| {
+        floor += switch (c) {
+            '(' => 1,
+            ')' => -1,
+            '\n' => break,
+            else => unreachable,
+        };
+
+        if (floor < 0) {
+            return i;
+        }
+    }
+    unreachable;
+}
+
 pub fn main() !void {
     const input = @embedFile("input.txt");
-    const result = part_1(input);
-    std.debug.print("part 1 result: {}\n", .{result});
+    {
+        const result = part_1(input);
+        std.debug.print("part 1 result: {}\n", .{result});
+    }
+    {
+        const result = part_2(input);
+        std.debug.print("part 2 result: {}\n", .{result});
+    }
 }
 
 const testing = std.testing;
@@ -31,4 +54,9 @@ test "part 1 example input" {
     try testing.expectEqual(part_1("))("), -1);
     try testing.expectEqual(part_1(")))"), -3);
     try testing.expectEqual(part_1(")())())"), -3);
+}
+
+test "part 2 example input" {
+    try testing.expectEqual(part_2(")"), 1);
+    try testing.expectEqual(part_2("()())"), 5);
 }
