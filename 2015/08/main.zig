94a305009569c86d1e02fe30f61025c02a1d1d87
Andrew Rogers <andrurogerz@gmail.com>
2024-01-29 10:04:32 -0800

    2015 day 08 part 2 solution

--- 2015/08/main.zig
+++ 2015/08/main.zig
@@ -3,8 +3,14 @@ const std = @import("std");
 
 pub fn main() !void {
     const input = @embedFile("input.txt");
-    const result = part_1(input);
-    std.debug.print("part 1 result: {}\n", .{result});
+    {
+        const result = part_1(input);
+        std.debug.print("part 1 result: {}\n", .{result});
+    }
+    {
+        const result = part_2(input);
+        std.debug.print("part 2 result: {}\n", .{result});
+    }
 }
 
 fn part_1(input: []const u8) usize {
@@ -45,6 +51,36 @@ fn part_1(input: []const u8) usize {
     return code_size - mem_size;
 }
 
+fn part_2(input: []const u8) usize {
+    var code_size: usize = 0;
+    var encoded_size: usize = 0;
+
+    for (input) |ch| {
+        switch (ch) {
+            ' ', '\t', '\r', '\x0B', '\x0C' => {}, // ignore whitespace
+            '\n' => {
+                // Every line is re-quoted ""
+                encoded_size += 2;
+            },
+            '\\', '\"' => {
+                code_size += 1;
+                encoded_size += 2;
+            },
+            'a'...'z', 'A'...'Z', '0'...'9' => {
+                code_size += 1;
+                encoded_size += 1;
+            },
+            else => unreachable,
+        }
+    }
+
+    if (input[input.len - 1] != '\n') {
+        // Compensate for input that doesn't end on a newline.
+        encoded_size += 2;
+    }
+    return encoded_size - code_size;
+}
+
 const testing = std.testing;
 
 test "part 1 example input" {
@@ -56,4 +92,15 @@ test "part 1 example input" {
     ;
 
     try testing.expectEqual(part_1(EXAMPLE_INPUT), 12);
+}
+
+test "part 2 example input" {
+    const EXAMPLE_INPUT =
+        \\""
+        \\"abc"
+        \\"aaa\"aaa"
+        \\"\x27"
+    ;
+
+    try testing.expectEqual(part_2(EXAMPLE_INPUT), 19);
 }
