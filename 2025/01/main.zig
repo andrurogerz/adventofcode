e03e06e20d3f54673326cbc08d9e6de7c1821e92
Andrew Rogers <andrurogerz@gmail.com>
2025-12-03 14:53:02 -0800

    2025 day 01 part 2 solution

--- 2025/01/main.zig
+++ 2025/01/main.zig
@@ -7,6 +7,10 @@ pub fn main() !void {
         const result = try part_1(input);
         std.debug.print("part 1 result: {}\n", .{result});
     }
+    {
+        const result = try part_2(input);
+        std.debug.print("part 2 result: {}\n", .{result});
+    }
 }
 
 pub fn part_1(input: []const u8) !usize {
@@ -15,14 +19,49 @@ pub fn part_1(input: []const u8) !usize {
     var cmd_iter = std.mem.tokenizeSequence(u8, input, "\n");
     while (cmd_iter.next()) |cmd| {
         if (cmd.len < 2) return error.InvalidFormat;
-        const rotation: i64 = @intCast(try std.fmt.parseInt(usize, cmd[1..], 10));
+        const rotation = try std.fmt.parseInt(usize, cmd[1..], 10);
         dial_position = switch (cmd[0]) {
-            'R' => @mod((dial_position + rotation), @as(i64, 100)),
-            'L' => @mod((dial_position - rotation), @as(i64, 100)),
+            'R' => dial_position + @as(i64, @intCast(rotation)),
+            'L' => dial_position - @as(i64, @intCast(rotation)),
             else => return error.InvalidFormat,
         };
+        dial_position = @mod(dial_position, 100);
         zero_position_count += if (dial_position == 0) 1 else 0;
     }
+    return zero_position_count;
+}
+
+pub fn part_2(input: []const u8) !usize {
+    var dial_position: i64 = 50;
+    var zero_position_count: usize = 0;
+    var cmd_iter = std.mem.tokenizeSequence(u8, input, "\n");
+    while (cmd_iter.next()) |cmd| {
+        if (cmd.len < 2) return error.InvalidFormat;
+        const rotation = try std.fmt.parseInt(usize, cmd[1..], 10);
+
+        // Account for the number of full rotations up-front.
+        const full_rotations = rotation / 100;
+        const partial_rotation = rotation % 100;
+
+        const dial_adjust = switch (cmd[0]) {
+            'R' => dial_position + @as(i64, @intCast(partial_rotation)),
+            'L' => dial_position - @as(i64, @intCast(partial_rotation)),
+            else => return error.InvalidFormat,
+        };
+
+        const new_dial_position = @mod(dial_adjust, 100);
+
+        zero_position_count += full_rotations;
+
+        if (dial_position != 0) {
+            // If it started at any position other than zero, we may have passed zero one additional
+            // time or landed on it exactly. Account for that case here by checking for wrap-around.
+            zero_position_count += if (new_dial_position == 0 or (dial_adjust != new_dial_position)) 1 else 0;
+        }
+
+        dial_position = new_dial_position;
+    }
+
     return zero_position_count;
 }
 
@@ -43,4 +82,8 @@ const EXAMPLE_INPUT =
 
 test "part 1 example input" {
     try testing.expectEqual(part_1(EXAMPLE_INPUT), 3);
+}
+
+test "part 2 example input" {
+    try testing.expectEqual(part_2(EXAMPLE_INPUT), 6);
 }
