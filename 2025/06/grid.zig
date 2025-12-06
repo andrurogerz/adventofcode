369d1fbce9c3a1bbccd7e03cf35ee84b7e380481
Andrew Rogers <andrurogerz@gmail.com>
2025-12-06 12:34:56 -0800

    2025 day 06 part 2 solution

---
+++ 2025/06/grid.zig
@@ -0,0 +1,161 @@
+const std = @import("std");
+
+/// Parses a grid in text form with uniform row length and provides a read-only
+/// interface to access its contents.
+pub fn Grid(comptime N: usize) type {
+    return struct {
+        const Self = @This();
+
+        /// An addressible position within a grid.
+        pub const Position = struct {
+            col: usize,
+            row: usize,
+        };
+
+        /// Iterates the valid grid positions surrounding the specified position.
+        pub const NeighborIterator = struct {
+            origin: Position,
+            cur_pos: Position,
+            start_pos: Position,
+            end_pos: Position,
+
+            fn init(pos: Position, grid: *const Grid(N)) @This() {
+                const origin = pos;
+                var start_pos = pos;
+                var end_pos = pos;
+
+                if (start_pos.row > 0) {
+                    start_pos.row -= 1;
+                }
+
+                if (end_pos.row < grid.rows - 1) {
+                    end_pos.row += 1;
+                }
+
+                if (start_pos.col > 0) {
+                    start_pos.col -= 1;
+                }
+
+                if (end_pos.col < grid.cols - 1) {
+                    end_pos.col += 1;
+                }
+
+                return @This(){
+                    .origin = origin,
+                    .cur_pos = start_pos,
+                    .start_pos = start_pos,
+                    .end_pos = end_pos,
+                };
+            }
+
+            pub fn next(self: *@This()) ?Position {
+                if (self.cur_pos.row > self.end_pos.row) {
+                    return null;
+                }
+
+                const pos = self.cur_pos;
+                self.cur_pos.col += 1;
+                if (self.cur_pos.col > self.end_pos.col) {
+                    self.cur_pos.col = self.start_pos.col;
+                    self.cur_pos.row += 1;
+                }
+
+                if (self.origin.row == pos.row and self.origin.col == pos.col) {
+                    return self.next(); // skip the origin point during enumeration
+                }
+                return pos;
+            }
+        };
+
+        /// Iterates every valid position in the grid one at a time.
+        pub const Iterator = struct {
+            cur_pos: Position,
+            end_pos: Position,
+
+            pub fn init(grid: *const Grid(N)) @This() {
+                return @This(){
+                    .cur_pos = .{ .row = 0, .col = 0 },
+                    .end_pos = .{ .row = grid.rows - 1, .col = grid.cols - 1 },
+                };
+            }
+
+            pub fn next(self: *@This()) ?Position {
+                if (self.cur_pos.row > self.end_pos.row) {
+                    return null;
+                }
+
+                const pos = self.cur_pos;
+                self.cur_pos.col += 1;
+                if (self.cur_pos.col > self.end_pos.col) {
+                    self.cur_pos.col = 0;
+                    self.cur_pos.row += 1;
+                }
+                return pos;
+            }
+        };
+
+        cols: usize,
+        rows: usize,
+        data: [N]u8,
+
+        pub fn init(data: []const u8) Self {
+            std.debug.assert(data.len <= N);
+
+            // Establish width/height of the grid.
+            var cols: usize = 0;
+            var rows: usize = 0;
+            for (data, 0..) |ch, idx| {
+                if (ch != '\n') {
+                    continue;
+                }
+
+                rows += 1;
+                if (cols == 0) {
+                    cols = idx;
+                    continue;
+                }
+
+                // Every row in the input data must be the same length.
+                std.debug.assert((idx + 1) % (cols + 1) == 0);
+            }
+
+            var result = Self{
+                .rows = rows,
+                .cols = cols,
+                .data = undefined,
+            };
+
+            @memcpy(&result.data, data);
+            return result;
+        }
+
+        /// Get the character at the specified grid position.
+        pub fn get(self: *const Self, pos: Position) u8 {
+            return self.data[self.getIndex(pos)];
+        }
+
+        /// Set the character at the specified grid position.
+        pub fn set(self: *Self, pos: Position, ch: u8) void {
+            self.data[self.getIndex(pos)] = ch;
+        }
+
+        /// Return an iterator to enumerate every valid position in the grid.
+        pub fn iterate(self: *const Self) Iterator {
+            return Iterator.init(self);
+        }
+
+        /// Return an iterator to enumerate every valid position in the grid
+        /// that is adjacent to the specified position.
+        pub fn neighbors(self: *const Self, pos: Position) NeighborIterator {
+            return NeighborIterator.init(pos, self);
+        }
+
+        /// Gets the unique index into the original string for the specified
+        /// grid position.
+        pub fn getIndex(self: *const Self, pos: Position) usize {
+            std.debug.assert(pos.col < self.cols);
+            std.debug.assert(pos.row < self.rows);
+            return pos.col + (pos.row * (self.cols + 1));
+        }
+    };
+}
--- 2025/06/main.zig
+++ 2025/06/main.zig
@@ -1,5 +1,6 @@
 //! https://adventofcode.com/2025/day/6
 const std = @import("std");
+const Grid = @import("grid.zig").Grid;
 
 pub fn main() !void {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
@@ -11,9 +12,13 @@ pub fn main() !void {
         const result = try part_1(allocator, input);
         std.debug.print("part 1 result: {}\n", .{result});
     }
+    {
+        const result = try part_2(allocator, input);
+        std.debug.print("part 2 result: {}\n", .{result});
+    }
 }
 
-pub fn part_1(allocator: std.mem.Allocator, input: []const u8) !usize {
+pub fn part_1(allocator: std.mem.Allocator, comptime input: []const u8) !usize {
     var numbers = std.ArrayList(usize).init(allocator);
     defer numbers.deinit();
 
@@ -68,15 +73,76 @@ pub fn part_1(allocator: std.mem.Allocator, input: []const u8) !usize {
     return result;
 }
 
+pub fn part_2(_: std.mem.Allocator, comptime input: []const u8) !usize {
+    @setEvalBranchQuota(20000);
+    const grid = comptime Grid(input.len).init(input);
+
+    // We can avoid heap-allocating this array because grid is comptime initialized.
+    var numbers = [_]usize{0} ** grid.cols;
+
+    // Parse the numbers in vertical columns. Assumes the data is well-formed and that there are no
+    // gaps between number characters in any given column. The implementation does not enforce this
+    // property because it would require additional overhead.
+    for (0..grid.cols) |col_idx| {
+        for (0..(grid.rows - 1)) |row_idx| {
+            const ch = grid.get(.{ .row = row_idx, .col = col_idx });
+            if (ch == ' ') continue;
+            if (ch < '0' or ch > '9') return error.InvalidInput;
+            numbers[col_idx] *= 10;
+            numbers[col_idx] += ch - '0';
+        }
+    }
+
+    // Extract the last line of the input from the grid and tokenize it. It should consist entirely
+    // of + and * characters, separated by spaces.
+    const last_line_idx = comptime grid.getIndex(.{ .col = 0, .row = grid.rows - 1 });
+    var op_iter = std.mem.tokenizeAny(u8, grid.data[last_line_idx..], "\n ");
+
+    var result: usize = 0;
+    var number_idx: usize = 0;
+    while (op_iter.next()) |op_str| {
+        // Input must be a sequence of * and + chars separated by spaces.
+        if (op_str.len != 1) return error.InvalidInput;
+        const op: enum { Multiply, Add } = switch (op_str[0]) {
+            '*' => .Multiply,
+            '+' => .Add,
+            else => return error.InvalidInput,
+        };
+
+        var col_result: usize = switch (op) {
+            .Multiply => 1,
+            .Add => 0,
+        };
+
+        while (number_idx < numbers.len and numbers[number_idx] != 0) {
+            const number = numbers[number_idx];
+            col_result = switch (op) {
+                .Multiply => col_result * number,
+                .Add => col_result + number,
+            };
+            number_idx += 1;
+        }
+
+        result += col_result;
+        number_idx += 1;
+    }
+    return result;
+}
+
 const testing = std.testing;
 
 const EXAMPLE_INPUT =
     \\123 328  51 64 
-    \\45 64  387 23 
-    \\6 98  215 314
+    \\ 45 64  387 23 
+    \\  6 98  215 314
     \\*   +   *   +  
+    \\
 ;
 
 test "part 1 example input" {
     try testing.expectEqual(part_1(testing.allocator, EXAMPLE_INPUT), 4277556);
+}
+
+test "part 2 example input" {
+    try testing.expectEqual(part_2(testing.allocator, EXAMPLE_INPUT), 3263827);
 }
