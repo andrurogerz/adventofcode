b1830881659cd96b9ffb622bb48c32322634ec95
Andrew Rogers <andrurogerz@gmail.com>
2025-12-09 16:30:32 -0800

    2025 day 09 part 2 solution

--- 2025/09/main.zig
+++ 2025/09/main.zig
@@ -7,6 +7,10 @@ pub fn main() !void {
         const result = try part_1(input);
         std.debug.print("part 1 result: {}\n", .{result});
     }
+    {
+        const result = try part_2(input);
+        std.debug.print("part 2 result: {}\n", .{result});
+    }
 }
 
 const Tile = struct {
@@ -22,10 +26,76 @@ pub fn part_1(comptime input: []const u8) !usize {
         for ((tile_1_idx + 1)..tiles.len) |tile_2_idx| {
             const tile_1 = tiles[tile_1_idx];
             const tile_2 = tiles[tile_2_idx];
-            const x = if (tile_1.x > tile_2.x) (tile_1.x - tile_2.x) else (tile_2.x - tile_1.x);
-            const y = if (tile_1.y > tile_2.y) (tile_1.y - tile_2.y) else (tile_2.y - tile_1.y);
-            const area = (x + 1) * (y + 1);
+            const width = if (tile_1.x > tile_2.x) (tile_1.x - tile_2.x) else (tile_2.x - tile_1.x);
+            const height = if (tile_1.y > tile_2.y) (tile_1.y - tile_2.y) else (tile_2.y - tile_1.y);
+            const area = (width + 1) * (height + 1);
             if (area > largest_area) largest_area = area;
+        }
+    }
+    return largest_area;
+}
+
+pub fn part_2(comptime input: []const u8) !usize {
+    @setEvalBranchQuota(50000);
+    const tiles = try parse_input(comptime count_lines(input), input);
+    var largest_area: usize = 0;
+    for (0..tiles.len) |tile_1_idx| {
+        for ((tile_1_idx + 1)..tiles.len) |tile_2_idx| {
+            // Calculate the rectangle difined by the consecutive pair of tiles.
+            const x_min = @min(tiles[tile_1_idx].x, tiles[tile_2_idx].x);
+            const x_max = @max(tiles[tile_1_idx].x, tiles[tile_2_idx].x);
+            const y_min = @min(tiles[tile_1_idx].y, tiles[tile_2_idx].y);
+            const y_max = @max(tiles[tile_1_idx].y, tiles[tile_2_idx].y);
+
+            var intersect = false;
+            for (0..tiles.len) |tile_idx| {
+                // Consider each consecutive pair of tiles a line (with the last tile connecting
+                // back to the first). If any line intersects with a rectangle edge, it is not a
+                // valid solution.
+                const tile_1 = tiles[tile_idx];
+                const tile_2 = if (tile_idx < tiles.len - 1) tiles[tile_idx + 1] else tiles[0];
+
+                if (tile_1.y == tile_2.y and tile_1.y > y_min and tile_1.y < y_max) {
+                    // The line between the two tiles is horizontal.
+                    if (tile_1.x == tile_2.x) return error.InvalidInput;
+
+                    // Determine if any point on the horizontal line falls inside the rectangle by
+                    // checking if it intersects the left or right side.
+                    if ((tile_1.x > x_min and tile_1.x < x_max) or //
+                        (tile_2.x > x_min and tile_2.x < x_max) or //
+                        (tile_1.x <= x_min and tile_2.x >= x_max) or //
+                        (tile_2.x <= x_min and tile_1.x >= x_max))
+                    {
+                        intersect = true;
+                        break;
+                    }
+                }
+
+                if (tile_1.x == tile_2.x and tile_1.x > x_min and tile_1.x < x_max) {
+                    // The line between the two tiles is vertical.
+                    if (tile_1.y == tile_2.y) return error.InvalidInput;
+
+                    // Determine if any point on the vertical line falls inside the rectangle by
+                    // checking if it intersects the top or bottom.
+                    if ((tile_1.y > y_min and tile_1.y < y_max) or //
+                        (tile_2.y > y_min and tile_2.y < y_max) or //
+                        (tile_1.y <= y_min and tile_2.y >= y_max) or //
+                        (tile_2.y <= y_min and tile_1.y >= y_max))
+                    {
+                        intersect = true;
+                        break;
+                    }
+                }
+            }
+
+            if (intersect) continue;
+
+            const height = y_max - y_min;
+            const width = x_max - x_min;
+            const area = (width + 1) * (height + 1);
+            if (area > largest_area) {
+                largest_area = area;
+            }
         }
     }
     return largest_area;
@@ -48,9 +118,16 @@ fn parse_input(comptime N: usize, input: []const u8) ![N]Tile {
         tiles[tile_idx].x = try std.fmt.parseInt(usize, val_iter.next() orelse return error.InvalidInput, 10);
         tiles[tile_idx].y = try std.fmt.parseInt(usize, val_iter.next() orelse return error.InvalidInput, 10);
         if (val_iter.next()) |_| return error.InvalidInput;
+        // Consecutive tiles must be either in the same column or the same row.
+        if (tile_idx > 0 and !(tiles[tile_idx - 1].x == tiles[tile_idx].x or tiles[tile_idx - 1].y == tiles[tile_idx].y)) {
+            return error.InvalidInput;
+        }
         tile_idx += 1;
     }
     if (tile_idx != N) return error.InvalidInput;
+
+    // The list wraps, so the last tile must be either in the same column or row as the first.
+    if (!(tiles[N - 1].x == tiles[0].x or tiles[N - 1].y == tiles[0].y)) return error.InvalidInput;
     return tiles;
 }
 
@@ -70,4 +147,8 @@ const EXAMPLE_INPUT =
 
 test "part 1 example input" {
     try testing.expectEqual(part_1(EXAMPLE_INPUT), 50);
+}
+
+test "part 2 example input" {
+    try testing.expectEqual(part_2(EXAMPLE_INPUT), 24);
 }
